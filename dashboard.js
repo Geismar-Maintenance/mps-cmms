29c548bc09c5d8a8727982244de8052a6cb4307e
Geismar-Maintenance <damon.tinsley@mauserpackaging.com>
2026-06-04 09:06:53 -0500

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -252,7 +252,7 @@ function renderRuntimeTable() {
 
 <td class="pm-trigger">
   ${
-    asset.weeks_to_pm !== null && asset.weeks_to_pm <= 4
+    asset.weeks_to_pm !== null && !isNaN(Number(asset.weeks_to_pm)) && Number(asset.weeks_to_pm) <= 4
       ? `<span class="badge bg-warning text-dark">
            ${Number(asset.weeks_to_pm).toFixed(1)} wk
          </span>`
