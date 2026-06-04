353d00124b41c1c705a0d40a517b41bca307b3d1
Geismar-Maintenance <damon.tinsley@mauserpackaging.com>
2026-06-04 08:37:42 -0500

    Update PM trigger display in dashboard
    Replace future trigger dot with conditional badge display for weeks to PM.

--- dashboard.js
+++ dashboard.js
@@ -250,9 +250,15 @@ function renderRuntimeTable() {
           ${asset.ytd}
         </td>
 
-        <td class="pm-trigger">
-          <!-- Future trigger dot -->
-        </td>
+<td class="pm-trigger">
+  ${
+    asset.weeks_to_pm !== null && asset.weeks_to_pm <= 4
+      ? `<span class="badge bg-warning text-dark">
+           ${asset.weeks_to_pm.toFixed(1)} wk
+         </span>`
+      : ""
+  }
+</td>
       </tr>
     `;
 
